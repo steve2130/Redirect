27c92638c9906cdc538a2c6ea58eb8951db414ba
Tom <85768907+steve2130@users.noreply.github.com>
2024-10-13 01:22:17 +0800

    added a prototype and organized stuff

---
+++ src/Twitter_scraping_image.py
@@ -0,0 +1,14 @@
+# Objective: Scrap image(s) from a tweet, download it to local, and adding the credit to the image(s)' metadata 
+# Version: 0.0.1
+
+import asyncio
+from twscrape import API,gather
+
+async def main():
+    print()
+
+async def 
+
+
+if __name__ == "__main__":
+    main()
